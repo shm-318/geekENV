517bcad54e5cdd8e301ad5f20eaea127db7af2bd
shm-318 <shivam318@gmail.com>
2021-07-13 22:55:19 +0530

    About and contact pages added

--- Techblog/blog/models.py
+++ Techblog/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    Name = models.CharField(max_length=200)
+    Email = models.EmailField(max_length=200)
+    ContactNo= models.PositiveBigIntegerField()
+    Message = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
