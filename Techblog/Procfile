bfd76ac803c1ded1a99ffed48b833bc3f5a43cd5
KumarSuraj <surajk.ug19.ec@nitp.ac.in>
2022-07-29 23:12:03 +0530

    code added

---
+++ Techblog/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Techblog.wsgi
--- Techblog/Techblog/settings.py
+++ Techblog/Techblog/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = os.path.join(str(BASE_DIR), 'static')
